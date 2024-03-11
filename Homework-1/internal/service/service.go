d375bc25dc259104757f32e82748e5c6b9d573f2
Дмитрий <dimavyatskin@gmail.com>
2024-03-11 13:00:25 +0700

    [GOHW-1]: Move business logic from storage.go to service.go

--- Homework-1/internal/service/service.go
+++ Homework-1/internal/service/service.go
@@ -2,17 +2,16 @@ package service
 
 import (
 	"Homework-1/internal/model"
+	storage2 "Homework-1/internal/storage"
 	"errors"
 	"fmt"
+	"time"
 )
 
 type storage interface {
 	Create(order model.OrderInput) error
-	Delete(id int) error
-	List(customerId int, limit int, onlyNotFinished bool) ([]model.Order, error)
-	Returns() ([]model.Order, error)
-	Finish(ids []int) error
-	Return(id int, customerId int) error
+	GetAllOrders() []storage2.OrderDTO
+	SaveChanges() error
 }
 
 type Service struct {
@@ -25,60 +24,173 @@ func New(s storage) Service {
 
 // Create ...
 func (s Service) Create(input model.OrderInput) error {
+	orders := s.storage.GetAllOrders()
+	for _, i := range orders {
+		if input.ID == i.ID {
+			return errors.New("заказ с этим id уже есть в базе")
+		}
+	}
 	return s.storage.Create(input)
 }
 
 // Delete ...
 func (s Service) Delete(id int) error {
-	if id == 0 {
-		return errors.New("нулевой id цели")
+	orders := s.storage.GetAllOrders()
+	found := false
+	for indx, order := range orders {
+		if !order.IsDeleted && order.ID == id {
+			if order.IsFinished {
+				return errors.New("этот заказ уже был выдан получателю")
+			}
+			if order.ExpireDate.After(time.Now()) {
+				return errors.New("срок хранения этого заказа ещё не истёк")
+			}
+			orders[indx].IsDeleted = true
+			found = true
+			break
+		}
 	}
-	return s.storage.Delete(id)
-}
+	if !found {
+		return errors.New("заказ с данным id не найден")
+	}
 
-// List ...
-func (s Service) List(customerId int, limit int, onlyNotFinished bool) ([]model.Order, error) {
-	orders, err := s.storage.List(customerId, limit, onlyNotFinished)
-	if err != nil {
-		return nil, err
-	}
-	return orders, nil
+	return s.storage.SaveChanges()
 }
 
 // Return ...
 func (s Service) Return(id int, customerId int) error {
-	return s.storage.Return(id, customerId)
+	orders := s.storage.GetAllOrders()
+	found := false
+	for indx, order := range orders {
+		if !order.IsDeleted && order.ID == id {
+			if order.IsReturnedByClient {
+				return errors.New("возврат этого заказа уже был принят")
+			}
+			if order.CustomerID != customerId {
+				return errors.New("id клиента, возвращающего заказ, не совпадает с id получателя")
+			}
+			if !order.IsFinished {
+				return errors.New("этот заказ ещё не был выдан получателю")
+			}
+			if time.Now().After(order.DateFinished.Add(24 * time.Hour * 2)) {
+				return errors.New("прошло уже более 2-х дней с момента выдачи заказа")
+			}
+			orders[indx].IsReturnedByClient = true
+			found = true
+			break
+		}
+	}
+	if !found {
+		return errors.New("заказ с данным id не найден")
+	}
+	return s.storage.SaveChanges()
+}
+
+// Finish ...
+func (s Service) Finish(ids []int) error {
+	customerId := 0
+	orders := s.storage.GetAllOrders()
+	// TODO Использовать вложенные циклы неэффективно, переделать с использованием множества
+	for _, id := range ids {
+		found := false
+		for _, order := range orders {
+			if !order.IsDeleted && order.ID == id {
+				if customerId == 0 {
+					customerId = order.CustomerID
+				} else {
+					if customerId != order.CustomerID {
+						return errors.New("не все заказы принадлежат одному клиенту")
+					}
+				}
+
+				if order.IsFinished {
+					return errors.New("некоторые из заказов уже были выданы клиенту")
+				}
+
+				if order.ExpireDate.Before(time.Now()) {
+					return errors.New("у некоторых из заказов истёк срок хранения")
+				}
+				found = true
+				break
+			}
+		}
+		if !found {
+			return errors.New("некоторые из заказов с заданными id не найдены")
+		}
+
+	}
+
+	// TODO Использовать вложенные циклы неэффективно, переделать с использованием множества
+	for _, id := range ids {
+		for indx, order := range orders {
+			if order.ID == id {
+				orders[indx].IsFinished = true
+				orders[indx].DateFinished = time.Now()
+				break
+			}
+		}
+	}
+
+	return s.storage.SaveChanges()
+}
+
+// List ...
+func (s Service) List(customerId int, limit int, onlyNotFinished bool) ([]model.Order, error) {
+	orders := s.storage.GetAllOrders()
+	customer_orders := make([]model.Order, 0, len(orders))
+	for i := len(orders) - 1; i >= 0; i-- {
+		order := orders[i]
+		if !order.IsDeleted && order.CustomerID == customerId && (!onlyNotFinished || !order.IsFinished) {
+			customer_orders = append(customer_orders, model.Order{
+				ID:                 order.ID,
+				CustomerID:         order.CustomerID,
+				ExpireDate:         order.ExpireDate,
+				IsFinished:         order.IsFinished,
+				DateFinished:       order.DateFinished,
+				IsReturnedByClient: order.IsReturnedByClient,
+				IsDeleted:          order.IsDeleted,
+			})
+
+			if limit > 0 && len(customer_orders) == limit {
+				break
+			}
+		}
+
+	}
+	return customer_orders, nil
 }
 
 // Returns ...
 func (s Service) Returns(resultsPerPage int) (string, error) {
-	orders, err := s.storage.Returns()
-	if err != nil {
-		return "", err
+	orders := s.storage.GetAllOrders()
+	returned := make([]model.Order, 0, len(orders))
+	for _, order := range orders {
+		if !order.IsDeleted && order.IsReturnedByClient {
+			returned = append(returned, model.Order{
+				ID:                 order.ID,
+				CustomerID:         order.CustomerID,
+				ExpireDate:         order.ExpireDate,
+				IsFinished:         order.IsFinished,
+				DateFinished:       order.DateFinished,
+				IsReturnedByClient: order.IsReturnedByClient,
+				IsDeleted:          order.IsDeleted,
+			})
+		}
 	}
 
 	paginatedReturns := ""
 	count := 0
-	for _, order := range orders {
+	for _, order := range returned {
 		if count%resultsPerPage == 0 {
 			paginatedReturns += fmt.Sprintf("Страница %d\n", (count/resultsPerPage + 1))
 		}
 		s := fmt.Sprintf("id заказа: %d, ", order.ID)
 		s += fmt.Sprintf("выдан, дата выдачи: %d-%d-%d", order.DateFinished.Year(), order.DateFinished.Month(), order.DateFinished.Day())
 		s += ", клиент оформил возврат"
-
-		fmt.Println(s)
 		paginatedReturns += s + "\n"
 
 		count += 1
 	}
-	return paginatedReturns, nil
-}
 
-func (s Service) Finish(ids []int) error {
-	err := s.storage.Finish(ids)
-	if err != nil {
-		return err
-	}
-	return nil
+	return paginatedReturns, nil
 }
--- Homework-1/internal/storage/storage.go
+++ Homework-1/internal/storage/storage.go
@@ -4,10 +4,8 @@ import (
 	"Homework-1/internal/model"
 	"bufio"
 	"encoding/json"
-	"errors"
 	"io"
 	"os"
-	"time"
 )
 
 const storageName = "storage"
@@ -17,208 +15,7 @@ type Storage struct {
 	orders  []OrderDTO
 }
 
-func New() (Storage, error) {
-	file, err := os.OpenFile(storageName, os.O_CREATE, 0777)
-	if err != nil {
-		return Storage{}, err
-	}
-	ords, err := listAll(file)
-	return Storage{storage: file, orders: ords}, nil
-
-}
-
-// Create creates order
-func (s *Storage) Create(input model.OrderInput) error {
-
-	for _, i := range s.orders {
-		if input.ID == i.ID {
-			return errors.New("заказ с этим id уже есть в базе")
-		}
-	}
-	newOrder := OrderDTO{
-		ID:         input.ID,
-		CustomerID: input.CustomerID,
-		ExpireDate: input.ExpireDate,
-	}
-
-	s.orders = append(s.orders, newOrder)
-	err := writeBytes(s.orders)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func writeBytes(orders []OrderDTO) error {
-	rawBytes, err := json.Marshal(orders)
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(storageName, rawBytes, 0777)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Delete deletes an order
-func (s *Storage) Delete(id int) error {
-
-	found := false
-	for indx, order := range s.orders {
-		if order.ID == id {
-			if order.IsFinished {
-				return errors.New("этот заказ уже был выдан получателю")
-			}
-			if order.ExpireDate.After(time.Now()) {
-				return errors.New("срок хранения этого заказа ещё не истёк")
-			}
-			s.orders[indx].IsDeleted = true
-			found = true
-		}
-	}
-	if !found {
-		return errors.New("заказ с данным id не найден")
-	}
-	err := writeBytes(s.orders)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Return set the target order IsReturnedByClient status to True
-func (s *Storage) Return(id int, customer_id int) error {
-
-	found := false
-	for indx, order := range s.orders {
-		if order.ID == id {
-			if order.IsReturnedByClient {
-				return errors.New("возврат этого заказа уже был принят")
-			}
-			if order.CustomerID != customer_id {
-				return errors.New("id клиента, возвращающего заказ, не совпадает с id получателя")
-			}
-			if !order.IsFinished {
-				return errors.New("этот заказ ещё не был выдан получателю")
-			}
-			if time.Now().After(order.DateFinished.Add(24 * time.Hour * 2)) {
-				return errors.New("прошло уже более 2-х дней с момента выдачи заказа")
-			}
-			s.orders[indx].IsReturnedByClient = true
-			found = true
-		}
-	}
-	if !found {
-		return errors.New("заказ с данным id не найден")
-	}
-	err := writeBytes(s.orders)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Finish finishes an order
-func (s *Storage) Finish(ids []int) error {
-
-	customerId := 0
-	// TODO Использовать вложенные циклы неэффективно, переделать с использованием множества
-	for _, id := range ids {
-		found := false
-		for _, order := range s.orders {
-			if order.ID == id {
-				if customerId == 0 {
-					customerId = order.CustomerID
-				} else {
-					if customerId != order.CustomerID {
-						return errors.New("не все заказы принадлежат одному клиенту")
-					}
-				}
-
-				if order.IsFinished {
-					return errors.New("некоторые из заказов уже были выданы клиенту")
-				}
-
-				if order.ExpireDate.Before(time.Now()) {
-					return errors.New("у некоторых из заказов истёк срок хранения")
-				}
-				found = true
-			}
-		}
-		if !found {
-			return errors.New("некоторые из заказов с заданными id не найдены")
-		}
-
-	}
-
-	for _, id := range ids {
-		for indx, order := range s.orders {
-			if order.ID == id {
-				s.orders[indx].IsFinished = true
-				s.orders[indx].DateFinished = time.Now()
-			}
-		}
-	}
-
-	err := writeBytes(s.orders)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// List returns s.orders orders of the target user from storage
-func (s *Storage) List(customerId int, limit int, onlyNotFinished bool) ([]model.Order, error) {
-
-	customer_orders := make([]model.Order, 0, len(s.orders))
-	for i := len(s.orders) - 1; i >= 0; i-- {
-		order := s.orders[i]
-		if !order.IsDeleted && order.CustomerID == customerId && (!onlyNotFinished || !order.IsFinished) {
-			customer_orders = append(customer_orders, model.Order{
-				ID:                 order.ID,
-				CustomerID:         order.CustomerID,
-				ExpireDate:         order.ExpireDate,
-				IsFinished:         order.IsFinished,
-				DateFinished:       order.DateFinished,
-				IsReturnedByClient: order.IsReturnedByClient,
-				IsDeleted:          order.IsDeleted,
-				//Description: order.Description,
-			})
-
-			if limit > 0 && len(customer_orders) == limit {
-				break
-			}
-		}
-
-	}
-
-	return customer_orders, nil
-}
-
-// Returns returns s.orders returned orders from storage
-func (s *Storage) Returns() ([]model.Order, error) {
-
-	returned := make([]model.Order, 0, len(s.orders))
-	for _, order := range s.orders {
-		if order.IsReturnedByClient {
-			returned = append(returned, model.Order{
-				ID:                 order.ID,
-				CustomerID:         order.CustomerID,
-				ExpireDate:         order.ExpireDate,
-				IsFinished:         order.IsFinished,
-				DateFinished:       order.DateFinished,
-				IsReturnedByClient: order.IsReturnedByClient,
-				IsDeleted:          order.IsDeleted,
-			})
-		}
-	}
-
-	return returned, nil
-}
-
-func listAll(file *os.File) ([]OrderDTO, error) {
+func loadFile(file *os.File) ([]OrderDTO, error) {
 	reader := bufio.NewReader(file)
 	rawBytes, err := io.ReadAll(reader)
 	if err != nil {
@@ -234,4 +31,55 @@ func listAll(file *os.File) ([]OrderDTO, error) {
 	}
 
 	return orders, nil
+}
+
+func New() (Storage, error) {
+	file, err := os.OpenFile(storageName, os.O_CREATE, 0777)
+	if err != nil {
+		return Storage{}, err
+	}
+	ords, err := loadFile(file)
+	return Storage{storage: file, orders: ords}, nil
+
+}
+
+func writeBytes(orders []OrderDTO) error {
+	rawBytes, err := json.Marshal(orders)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(storageName, rawBytes, 0777)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Storage) SaveChanges() error {
+	err := writeBytes(s.orders)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Create creates order
+func (s *Storage) Create(input model.OrderInput) error {
+	newOrder := OrderDTO{
+		ID:         input.ID,
+		CustomerID: input.CustomerID,
+		ExpireDate: input.ExpireDate,
+	}
+	s.orders = append(s.orders, newOrder)
+	err := s.SaveChanges()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetAllOrders returns all orders
+func (s *Storage) GetAllOrders() []OrderDTO {
+	return s.orders
 }
