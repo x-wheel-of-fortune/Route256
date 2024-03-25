6c07cb004710fd8e4c003fae77492f5271aa53c5
Дмитрий <dimavyatskin@gmail.com>
2024-03-25 21:28:07 +0700

    [GOHW-3]: Update migration

--- Homework-1/internal/pkg/db/migrations/20240323105847_.sql
+++ Homework-1/internal/pkg/db/migrations/20240323105847_.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 create table pickup_points(
-    id BIGSERIAL PRIMARY KEY NOT NULL
-    name TEXT NOT NULL DEFAULT ''
-    address TEXT NOT NULL DEFAULT ''
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL DEFAULT '',
+    address TEXT NOT NULL DEFAULT '',
     phone_number TEXT NOT NULL DEFAULT ''
-)
+);
 -- +goose StatementEnd
 
 -- +goose Down
